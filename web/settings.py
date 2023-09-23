ac27ba5371180fc29a78357a79fa55e889bd3b62
hush-tukul <hush-tukul@protonmail.com>
2023-09-23 08:07:44 +0200

    Starter_Pack Alpha 1.2

--- web/settings.py
+++ web/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+env = Env() # new
+env.read_env() # new
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +80,9 @@ WSGI_APPLICATION = "web.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.dj_db_url("DATABASE_URL",
+    #default="postgres://postgres@db/postgres"
+    )
 }
 
 
